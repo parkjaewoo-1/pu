22738bbe3e9f1197642ee01ff6568ddb2b48e4c4
jaewoo <zmalqoqo@gmail.com>
2020-10-09 01:44:44 +0900

    비교하기 프로세스01

--- noonno/resource/js/design_common.js
+++ noonno/resource/js/design_common.js
@@ -81,11 +81,80 @@ function shareBtn(){
     });
 }
 function compareBox(){
-    $('.prd input').on('change', function(){
+    var inputEl = $(".prd input:checkbox[class='prdchkB']"),
+    ChkNum = 0, _img = [], _tit = [],
+    compList = $('.compare_box .list > div'),
+    compImgSort = $('.compare_box .list > div .img img'),
+    compTitSort = $('.compare_box .list > div .desc'),
+    compareBox = $('.compare_box'),
+    compareBoxTit = $('.compare_box > .tit'),
+    comparRemove = $('.compare_box > .desc > .list > div.on .del');
 
-        TweenMax.to('.compare_box', 0.6, {bottom:'0', ease:power4});
-        $('.compare_box').addClass('on');
-        $('.compare_box .tit').addClass('on');
+    inputEl.on('change', function(){
+        if($(this).is(':checked') == true){ // 체크
+
+            ChkNum += 1;
+            _img.push($(this).prev('.thumb').find('img').attr('src'));
+            _tit.push($(this).next('label').find('.title').text());
+
+            console.log(ChkNum);
+
+            if(ChkNum > 0){
+                compareBoxTit.addClass('on');
+                compareBox.addClass('on');
+                TweenMax.to(compareBox, 0.6, {bottom: '0px', ease:power4});
+            }
+            if(ChkNum > 3){
+                ChkNum -= 1;
+                alert('비교하기는 3개 제품까지만 가능합니다.');
+                _img.splice(3,1);
+                _tit.splice(3,1);
+                $(this).prop("checked", false);                
+            }
+            
+            for(var i = 0; i < ChkNum; i++){
+                compImgSort.eq(i).attr('src',_img[i]);
+                compTitSort.eq(i).text(_tit[i]);
+                compList.eq(i).addClass('on');
+            }
+
+        }else if($(this).is(':checked') == false){ // 체크해제
+
+            ChkNum -= 1;
+            var comTit = $(this).next('label').find('.title').text(),
+            comImg = $(this).prev('.thumb').find('img').attr('src'),
+            idxofNum = _tit.indexOf(comTit);
+
+            _tit.splice($.inArray(comTit, _tit),1);
+            _img.splice($.inArray(comImg, _img),1);
+
+            console.log(ChkNum);
+
+            if(ChkNum > 0){
+                compareBoxTit.addClass('on');
+                compareBox.addClass('on');
+                TweenMax.to(compareBox, 0.6, {bottom: '0px', ease:power4});
+            }
+            if(ChkNum <= 0){
+                compareBoxTit.removeClass('on');
+                compareBox.removeClass('on');
+                TweenMax.to(compareBox, 0.6, {bottom: '-350px', ease:power4});
+            }
+
+            for(var i = 0; i < ChkNum; i++){
+                compImgSort.eq(i).attr('src',_img[i]);
+                compTitSort.eq(i).text(_tit[i]);
+                compList.eq(i).addClass('on');
+            }
+
+
+        }
+        
+
+        // TweenMax.to('.compare_box', 0.6, {bottom:'0', ease:power4});
+        
+        // $('.compare_box').addClass('on');
+        // $('.compare_box .tit').addClass('on');
     });
      $('.compare_box > .tit').click(function(){
         var thP = $(this).parent('.compare_box');
@@ -94,13 +163,15 @@ function compareBox(){
             thP.removeClass('on');
             TweenMax.to(thP, 0.6, {bottom: '-350px', ease:power4});
             $('.tab_cont').removeClass('on')
+        }else{
+            $(this).addClass('on');
+            thP.addClass('on');
+            TweenMax.to(thP, 0.6, {bottom: '0px', ease:power4});
+            $('.tab_cont').addClass('on');
         }
 
      });
-     $('.compare_btn').click(function(){
-
-
-     });
+     
 
 
 }
