4ddba0e22d10f351105e1e927ec2da17b1e35364
jaewoo <zmalqoqo@gmail.com>
2020-10-13 01:17:58 +0900

    한눈에보기02 fild position

--- noonno/resource/js/products_map.js
+++ noonno/resource/js/products_map.js
@@ -1,5 +1,27 @@
 $(function(){
   
 });
+$(document).ready(function(){
+  mapPosition();
+});
+function mapPosition(){
+  var posArr = [],
+  posCont = $('.gr-width'),
+  _leng = $('#age-select-sort li'),
+  overWAl = $('#age-select-sort li.type2').position().left * 10 / 100;
+
+  // 100 * 10 / 100          10%
+
+  for(var i = 0; i < _leng.length ; i++){
+    var _thisPos = _leng.eq(i).position().left;
+    posArr.push(_thisPos);
+  }
+
+  posCont.each(function(i){
+    var posSort = $(this).attr('data-startpos');
+    $(this).css('left',posArr[posSort]);
+    
+  });
 
 
+}
